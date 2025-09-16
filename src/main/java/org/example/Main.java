eef35d344f64cefe8ba4a0e3c64a6f17707765f4
ToryHyena <peskovetsvv@gmail.com>
2025-09-16 14:59:10 +0300

    commit_12

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,111 @@
-package org.example;
+package triangle;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Triangle {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    final int TR_EQUILATERAL = 1; // ��������������
+    final int TR_ISOSCELES = 2;   // ��������������
+    final int TR_ORDYNARY = 4;    // �������
+    final int TR_RECTANGULAR = 8; // �������������
+
+
+    private double a;
+    private double b;
+    private double c;
+
+    private String message;
+
+    public Triangle(double a, double b, double c)
+    {
+        this.a=a;
+        this.b=b;
+        this.c=c;
+        this.message = "";
+    }
+
+    public String getMessage()
+    {
+        return this.message;
+    }
+
+    public boolean checkTriangle()
+    {
+        if (a<=0)
+        {
+            this.message = "a<=0";
+            return false;
+        }
+
+        if (b<=0)
+        {
+            this.message = "b<=0";
+            return false;
+        }
+
+        if (b<=0)
+        {
+            this.message = "c<=0";
+            return false;
+        }
+
+        if (a+b<=c)
+        {
+            this.message = "a+b<=c";
+            return false;
+        }
+
+        if (a+c<=b)
+        {
+            this.message = "a+c<=b";
+            return false;
+        }
+
+        if (b+c<=a)
+        {
+            this.message = "b+c<=a";
+            return false;
+        }
+
+        return true;
+    }
+
+    public int detectTriangle()
+    {
+        int final_state = 0;
+
+
+        if ((a*a+b*b == c*c) || (b*b + c*c == a*a)||(a*a + c*c == b*c))
+        {
+            final_state = final_state|TR_RECTANGULAR; // �������������
+        }
+
+
+        if (a==b && b==c && a==c)
+        {
+            final_state = final_state|TR_EQUILATERAL; // ��������������
+        }
+
+        if (a==b || b==c || a==c)
+        {
+            final_state = final_state|TR_ISOSCELES; // ��������������
+        }
+
+        if (final_state == 0)
+        {
+            return TR_ORDYNARY; // �������
+        }
+        else
+        {
+            return final_state; // ���������� ���������
         }
     }
+
+
+    public double getSquare()
+    {
+        double p;
+
+        p = (a+b+c)/2;
+        return Math.sqrt(p*(p-a)*(p-b)*(p-c));
+    }
+
 }
